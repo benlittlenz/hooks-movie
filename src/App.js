a657570e547bff411ff8eb04ada9112c74f8af0c
nzbenji <benlittle53@gmail.com>
2019-07-15 21:12:40 +1200

    Added semantic ui

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
 import React, {useEffect, useState} from 'react';
 import FetchAPIData from './FetchAPIData';
+import { Input, Button, Form, Icon, List, Grid, Image } from 'semantic-ui-react';
 
 const App = () => {
   const [query, setQuery] = useState('batman');
-  const [{ data, loading, error}, doFetch] = FetchAPIData([]);
+  const [{ data, loading, error}, performFetch] = FetchAPIData([]);
   const API_KEY = '4a3b711b'
   return(
     <div>
-      <form
+      <Form
         onSubmit={event => {
-          doFetch(`https://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`)
+          performFetch(`https://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`)
           event.preventDefault();
         }}
       >
-        <input
+      <span>
+        <Form.Input
           type="text"
+          placeholder="Search for a movie"
           value={query}
           onChange={(event) => setQuery(event.target.value)}
+          style={{ maxWidth: '40%', display: 'inline-block'}}
         />
-        <button type="submit">
-          Search
-        </button>
-      </form>
-      <ul>
+        <Button color='blue'>
+          <Icon name="search"  inverted link/>
+        </Button>
+      </span>
+      </Form>
+      <List style={{ textAlign: 'center'}}>
       {console.log(data)}
       {error && <div>Something went wrong...</div>}
+      <Grid.Row>
       {loading
-          ? <div>...Loading</div>
-          : data.map(movie => (
-            <span>
-              <img src={movie.Poster} />
-              <li>{movie.Title}</li>  
-            </span>
-           
-          ))
-        
+        ? <div>...Loading</div>
+        : data.map(movie => (
+            <Grid.Column>
+            <Image src={movie.Poster} />
+            <List.Item>{movie.Title}</List.Item>  
+            </Grid.Column>
+         
+        ))
       }
-      </ul>
+      </Grid.Row>
+      
+      </List>
     </div>
   )
 }
